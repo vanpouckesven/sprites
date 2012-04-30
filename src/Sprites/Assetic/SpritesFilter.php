1774abb50bd3bccdbf7aa7a7686dcec555d28d56
Pierre Minnieur <pm@pierre-minnieur.de>
2012-04-30 12:48:46 +0200

    initial commit for assetic support

---
+++ src/Sprites/Assetic/SpritesFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Sprites package.
+ *
+ * (c) Pierre Minnieur <pm@pierre-minnieur.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Sprites\Assetic;
+
+use Assetic\Filter\FilterInterface;
+use Sprites\Generator;
+
+class SpritesFilter implements FilterInterface
+{
+    /**
+     * @var Generator
+     */
+    private $generator;
+
+    /**
+     * Constructor.
+     *
+     * @param Generator $generator A Generator instance
+     */
+    public function __construct(Generator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filterLoad(AssetInterface $asset)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filterDump(AssetInterface $asset)
+    {
+    }
+}
---
+++ tests/Sprites/Tests/Assetic/SpritesFilterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Sprites package.
+ *
+ * (c) Pierre Minnieur <pm@pierre-minnieur.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Sprites\Tests\Assetic;
+
+use Sprites\Assetic\SpritesFilter;
+
+class SpritesFilterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilterLoad()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    public function testFilterDump()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
